15d9532338b58609a4b7e48e52de949ee6dec6a6
Guillaume Lessard <dhtnstff@gmail.com>
2020-04-13 17:49:48 -0600

    update conformances to AtomicInteger

--- Sources/SwiftCompatibleAtomics/AtomicInteger.swift
+++ Sources/SwiftCompatibleAtomics/AtomicInteger.swift
@@ -11,24 +11,4 @@
 //===----------------------------------------------------------------------===//
 
 public protocol AtomicInteger: AtomicProtocol, FixedWidthInteger
-{
-  static func atomicLoadThenWrappingIncrement(by operand: Self,
-                                              at pointer: UnsafeMutablePointer<AtomicStorage>,
-                                              ordering: AtomicUpdateOrdering) -> Self
-
-  static func atomicLoadThenWrappingDecrement(by operand: Self,
-                                              at pointer: UnsafeMutablePointer<AtomicStorage>,
-                                              ordering: AtomicUpdateOrdering) -> Self
-
-  static func atomicLoadThenBitwiseAnd(with operand: Self,
-                                       at pointer: UnsafeMutablePointer<AtomicStorage>,
-                                       ordering: AtomicUpdateOrdering) -> Self
-
-  static func atomicLoadThenBitwiseOr(with operand: Self,
-                                      at pointer: UnsafeMutablePointer<AtomicStorage>,
-                                      ordering: AtomicUpdateOrdering) -> Self
-
-  static func atomicLoadThenBitwiseXor(with operand: Self,
-                                       at pointer: UnsafeMutablePointer<AtomicStorage>,
-                                       ordering: AtomicUpdateOrdering) -> Self
-}
+  where AtomicStorage: PrimitiveAtomicInteger {}
