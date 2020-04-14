026919613fa12c13736dc1d9363062b827f0f8cb
Guillaume Lessard <dhtnstff@gmail.com>
2020-04-14 14:02:18 -0600

    adjust oldest usable tools version

--- Package@swift-4.1.swift
+++ Package@swift-4.1.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "SwiftCompatibleAtomics",
   products: [
-    .library(name: "SwiftCompatibleAtomics", targets: ["SwiftCompatibleAtomics"]),
+    .library(name: "SwiftCompatibleAtomics", type: .static, targets: ["SwiftCompatibleAtomics"]),
     .library(name: "CAtomicsPrimitives", type: .static, targets: ["CAtomicsPrimitives"]),
   ],
   targets: [
