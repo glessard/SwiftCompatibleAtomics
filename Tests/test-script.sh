60225800932427cf371e68e0b4fa638c6bef1097
Guillaume Lessard <dhtnstff@gmail.com>
2020-04-28 14:34:06 -0600

    remove unused version handling code from test script

--- Tests/test-script.sh
+++ Tests/test-script.sh
@@ -4,16 +4,6 @@ set -e
 COMPILER_MAJOR_VERSION=`echo ${COMPILER_VERSION} | awk -F . '{print $1}'`
 TEST_OPTIONS="-c release -Xcc -mcx16"
 
-if [[ "${COMPILER_MAJOR_VERSION}" = "3" ]]
-then
-  # this is version 3.1 of the swift compiler
-  # it doesn't handle SE-0151 right
-  swift package tools-version --set 3.1
-
-  # it doesn't handle testing in release configuration
-  TEST_OPTIONS="-Xcc -mcx16"
-fi
-
 swift --version
 swift test ${TEST_OPTIONS}
 
@@ -35,10 +25,6 @@ for LANGUAGE_VERSION in $VERSIONS
 do
   echo "" # add a small visual separation
   echo "Testing in compatibility mode for Swift ${LANGUAGE_VERSION}"
-  if [[ "${LANGUAGE_VERSION}" = "3" ]]
-  then
-    TEST_OPTIONS="-Xcc -mcx16"
-  fi
   swift package reset
   rm -f Package.resolved
   swift test ${TEST_OPTIONS} -Xswiftc -swift-version -Xswiftc ${LANGUAGE_VERSION}
