cba176d4842ec4566ec472468df13098f225cbce
Guillaume Lessard <dhtnstff@gmail.com>
2020-04-29 17:24:29 -0600

    atomic operations on pairs of values

--- Sources/SwiftCompatibleAtomics/UnsafeAtomicPair.swift
+++ Sources/SwiftCompatibleAtomics/UnsafeAtomicPair.swift
@@ -62,6 +62,14 @@ public struct UnsafeAtomicPair<E1: AtomicPairElement, E2: AtomicPairElement>
     storage.deallocate()
     return returned
   }
+
+  @usableFromInline
+  internal var atomic: UnsafeMutablePointer<AtomicDoubleWidth> {
+    @inlinable
+    get {
+      return UnsafeMutableRawPointer(storage).assumingMemoryBound(to: AtomicDoubleWidth.self)
+    }
+  }
 #else
   @_versioned
   internal let storage: UnsafeMutablePointer<Storage>
@@ -86,6 +94,14 @@ public struct UnsafeAtomicPair<E1: AtomicPairElement, E2: AtomicPairElement>
     let returned = storage.pointee.dispose()
     storage.deallocate()
     return returned
+  }
+
+  @_versioned
+  internal var atomic: UnsafeMutablePointer<AtomicDoubleWidth> {
+    @inline(__always)
+    get {
+      return UnsafeMutableRawPointer(storage).assumingMemoryBound(to: AtomicDoubleWidth.self)
+    }
   }
 #endif
 }
@@ -153,6 +169,123 @@ extension DoubleWidth
   {
     let dw = storage.decode()
     return (E1(decoding: dw.value1), E2(decoding: dw.value2))
+  }
+#endif
+}
+
+extension UnsafeAtomicPair
+{
+#if swift(>=4.2)
+  @inlinable
+  public func load(ordering: AtomicLoadOrdering) -> (E1, E2)
+  {
+    let encoded = CAtomicsLoad(atomic, ordering._rawValue)
+    return DoubleWidth.decodeAtomicStorage(encoded)
+  }
+
+  @inlinable
+  public func store(_ desired: (E1, E2), ordering: AtomicStoreOrdering)
+  {
+    let encoded = DoubleWidth.encodeAtomicStorage(desired)
+    CAtomicsStore(atomic, encoded, ordering._rawValue)
+  }
+
+  @inlinable
+  public func exchange(_ desired: (E1, E2), ordering: AtomicUpdateOrdering) -> (E1, E2)
+  {
+    let encoded = CAtomicsExchange(atomic, DoubleWidth.encodeAtomicStorage(desired), ordering._rawValue)
+    return DoubleWidth.decodeAtomicStorage(encoded)
+  }
+
+  @inlinable
+  public func compareExchange(expected: (E1, E2), desired: (E1, E2),
+                              ordering: AtomicUpdateOrdering) -> (exchanged: Bool, original: (E1, E2))
+  {
+    return compareExchange(expected: expected, desired: desired,
+                           successOrdering: ordering,
+                           failureOrdering: ordering.asLoadOrdering())
+  }
+
+  @inlinable
+  public func compareExchange(expected: (E1, E2), desired: (E1, E2),
+                              successOrdering: AtomicUpdateOrdering,
+                              failureOrdering: AtomicLoadOrdering) -> (exchanged: Bool, original: (E1, E2))
+  {
+    var expected = DoubleWidth.encodeAtomicStorage(expected)
+    let success = withUnsafeMutablePointer(to: &expected) {
+      CAtomicsCompareAndExchangeStrong(atomic, $0, DoubleWidth.encodeAtomicStorage(desired),
+                                       successOrdering._rawValue, failureOrdering._rawValue)
+    }
+    return (success, DoubleWidth.decodeAtomicStorage(expected))
+  }
+
+  @inlinable
+  public func weakCompareExchange(expected: (E1, E2), desired: (E1, E2),
+                                  successOrdering: AtomicUpdateOrdering,
+                                  failureOrdering: AtomicLoadOrdering) -> (exchanged: Bool, original: (E1, E2))
+  {
+    var expected = DoubleWidth.encodeAtomicStorage(expected)
+    let success = withUnsafeMutablePointer(to: &expected) {
+      CAtomicsCompareAndExchangeWeak(atomic, $0, DoubleWidth.encodeAtomicStorage(desired),
+                                     successOrdering._rawValue, failureOrdering._rawValue)
+    }
+    return (success, DoubleWidth.decodeAtomicStorage(expected))
+  }
+#else
+  @inline(__always)
+  public func load(ordering: AtomicLoadOrdering) -> (E1, E2)
+  {
+    let encoded = CAtomicsLoad(atomic, ordering._rawValue)
+    return DoubleWidth.decodeAtomicStorage(encoded)
+  }
+
+  @inline(__always)
+  public func store(_ desired: (E1, E2), ordering: AtomicStoreOrdering)
+  {
+    let encoded = DoubleWidth.encodeAtomicStorage(desired)
+    CAtomicsStore(atomic, encoded, ordering._rawValue)
+  }
+
+  @inline(__always)
+  public func exchange(_ desired: (E1, E2), ordering: AtomicUpdateOrdering) -> (E1, E2)
+  {
+    let encoded = CAtomicsExchange(atomic, DoubleWidth.encodeAtomicStorage(desired), ordering._rawValue)
+    return DoubleWidth.decodeAtomicStorage(encoded)
+  }
+
+  @inline(__always)
+  public func compareExchange(expected: (E1, E2), desired: (E1, E2),
+                              ordering: AtomicUpdateOrdering) -> (exchanged: Bool, original: (E1, E2))
+  {
+    return compareExchange(expected: expected, desired: desired,
+                           successOrdering: ordering,
+                           failureOrdering: ordering.asLoadOrdering())
+  }
+
+  @inline(__always)
+  public func compareExchange(expected: (E1, E2), desired: (E1, E2),
+                              successOrdering: AtomicUpdateOrdering,
+                              failureOrdering: AtomicLoadOrdering) -> (exchanged: Bool, original: (E1, E2))
+  {
+    var expected = DoubleWidth.encodeAtomicStorage(expected)
+    let success = withUnsafeMutablePointer(to: &expected) {
+      CAtomicsCompareAndExchangeStrong(atomic, $0, DoubleWidth.encodeAtomicStorage(desired),
+                                       successOrdering._rawValue, failureOrdering._rawValue)
+    }
+    return (success, DoubleWidth.decodeAtomicStorage(expected))
+  }
+
+  @inline(__always)
+  public func weakCompareExchange(expected: (E1, E2), desired: (E1, E2),
+                                  successOrdering: AtomicUpdateOrdering,
+                                  failureOrdering: AtomicLoadOrdering) -> (exchanged: Bool, original: (E1, E2))
+  {
+    var expected = DoubleWidth.encodeAtomicStorage(expected)
+    let success = withUnsafeMutablePointer(to: &expected) {
+      CAtomicsCompareAndExchangeWeak(atomic, $0, DoubleWidth.encodeAtomicStorage(desired),
+                                     successOrdering._rawValue, failureOrdering._rawValue)
+    }
+    return (success, DoubleWidth.decodeAtomicStorage(expected))
   }
 #endif
 }
